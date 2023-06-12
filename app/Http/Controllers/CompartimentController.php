30be76bc17a48deb49977b34a57f26b3baa1086c
Thomas Lamb <thomaslamb60@gmail.com>
2023-06-12 23:00:32 +0200

    Revert "Bug resolve on compartiments."
    This reverts commit e929bbb36fd7c6df8373092a87379aeb7064d8f3.

--- app/Http/Controllers/CompartimentController.php
+++ app/Http/Controllers/CompartimentController.php
@@ -3,21 +3,17 @@
 namespace App\Http\Controllers;
 
 use App\Models\Compartiment;
-use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use App\Http\Resources\CompartimentResource;
-use Illuminate\Http\Response;
-use Throwable;
 
 class CompartimentController extends Controller
 {
     /**
      * Display a listing of the resource.
      *
-     * @param Request $request
-     * @return Response
+     * @return \Illuminate\Http\Response
      */
-    public function index(Request $request): Response
+    public function index(Request $request)
     {
         return response(['data' => CompartimentResource::collection($request['bac']->compGetAll())], 200);
     }
@@ -25,8 +21,8 @@ class CompartimentController extends Controller
     /**
      * Store a newly created resource in storage.
      *
-     * @param Request $request
-     * @return void
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
     {
@@ -35,11 +31,10 @@ class CompartimentController extends Controller
     /**
      * Display the specified resource.
      *
-     * @param Request $request
-     * @param $compartiment
-     * @return JsonResponse
+     * @param  \App\Models\Compartiment  $compartiment
+     * @return \Illuminate\Http\Response
      */
-    public function show(Request $request, $compartiment): JsonResponse
+    public function show(Request $request, $compartiment)
     {
         $compartiment = $request['bac']->compGet($compartiment);
         return response()->json(['data' => new CompartimentResource($compartiment)]);
@@ -48,11 +43,11 @@ class CompartimentController extends Controller
     /**
      * Update the specified resource in storage.
      *
-     * @param Request $request
-     * @param $compNum
-     * @return JsonResponse
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Compartiment  $compartiment
+     * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $compNum): JsonResponse
+    public function update(Request $request, $compNum)
     {
         $inputs = $request->validate([
             'cap_hydro' => ['integer'],
@@ -63,7 +58,7 @@ class CompartimentController extends Controller
         $compartiment = $bac->compGet($compNum);
         try {
             $compartiment->update($inputs);
-        } catch (Throwable $th) {
+        } catch (\Throwable $th) {
             return response()->json(['message' => $th->getMessage()], 400);
         }
         return response()->json(['message' => 'Compartiment updated'], 201);
@@ -72,8 +67,8 @@ class CompartimentController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param Compartiment $compartiment
-     * @return void
+     * @param  \App\Models\Compartiment  $compartiment
+     * @return \Illuminate\Http\Response
      */
     public function destroy(Compartiment $compartiment)
     {
